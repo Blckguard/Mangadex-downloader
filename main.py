94bf64de68d7eaee887ef05efeb99f8575ccd054
Max <blckguard2000@gmail.com>
2023-02-18 14:59:14 +0900

    made some aesthetic changes.

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
-from requests import *
-from time import *
-from PyQt5.QtWidgets import *
+from time import sleep
+from requests import get
 
-url = input('manga url: ')
 
-def get_id(url):
-    # takes a url and returns an id for the given manga
-    # returns: string
 
-    return url.split('/')[4]
+def get_id():
+    ''' takes a url and returns an id for the given manga
+        returns: string '''
+
+    manga_url = input('manga url: ')
+    return manga_url.split('/')[4]
 
 def get_chapters():
-    # creates a list of chapters for the given manga
-    # returns: list
+    ''' creates a list of chapters for the given manga
+        returns: list'''
 
-    id = get_id(url)
-    chapters = []
+    manga_id = get_id()
+    chapters_list = []
     total = 1
     offset = 0
     successful = False
 
+    # config
+    order = ''
+    chapter = ''
+
     while not successful:
-        if len(chapters) == total:
+        if len(chapters_list) == total:
             successful = True
         else:
-            request_list = get(f'https://api.mangadex.org/chapter?manga={id}&limit=50&translatedLanguage[]=en&offset={offset}').json()
+            request_list = get(f'''https://api.mangadex.org/chapter?manga={manga_id}&offset={offset}{order}{chapter}&limit=50&translatedLanguage[]=en''').json()
+
+            if request_list['result'] == 'error':
+                print(request_list)
+                break
             offset += 50
             total = request_list['total']
-            chapters = chapters + request_list['data']
+            chapters_list += request_list['data']
             sleep(0.2)
-    return chapters
+    return chapters_list
+
 
 chapters = get_chapters()
 print(len(chapters))
-
